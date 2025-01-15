bdcf31ae8da53b3c67e608e55e66193280d892f1
Iago <iagopmodev@gmail.com>
2025-01-14 22:13:06 -0300

    configurando connection

--- Connection.php
+++ Connection.php
@@ -1,1 +1,21 @@
+<?php
+require 'vendor/autoload.php';
+use Dotenv\Dotenv;
 
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dbHost = $_ENV['DB_HOST'];
+$dbPort = $_ENV['DB_PORT'];
+$dbDatabase = $_ENV['DB_DATABASE'];
+$dbUserName = $_ENV['DB_USERNAME'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+
+try {
+    $pdo = new PDO(dsn: "mysql:host=$dbHost;port=$dbPort;dbname=$dbUserName;password=$dbPassword");
+    $pdo->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);    
+    return $pdo;
+} catch(PDOException $e) {
+    echo "Falha na conexão: " . $e->getMessage();
+    exit();    
+}
